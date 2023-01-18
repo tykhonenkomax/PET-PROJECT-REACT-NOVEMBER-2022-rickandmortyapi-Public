b99c7b894f38bad64a81aab089f967d7aad2729d
Max <tykhonenko.work@gmail.com>
2023-01-18 20:23:36 +0200

    getById from location2

--- src/components/locationDetails/LocationDetails.js
+++ src/components/locationDetails/LocationDetails.js
@@ -4,14 +4,14 @@ import {useLocation} from 'react-router-dom'
 
 const LocationDetails = () => {
 
-    const {state} = useLocation()
-    const {residents} = state;
+
+
 
     return (
 
         <div>
             {
-                residents.map(resident => (<div key={resident}>{resident}</div>))
+
             }
         </div>
     );
