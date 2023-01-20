f720dcf4474f4b36c5a87835b57e63af85bb44bd
Max <tykhonenko.work@gmail.com>
2023-01-20 22:48:59 +0200

    getById problem render in another component part2

--- src/components/episode/Episode.js
+++ src/components/episode/Episode.js
@@ -1,7 +1,7 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState, useMemo} from 'react';
 import {useSelector, useDispatch} from 'react-redux'
 import {locationAction} from '../../redux'
-import axios from 'axios'
+import {useGetLocationHook} from './hook'
 
 const Episode = ({episode}) => {
 
@@ -14,10 +14,19 @@ const Episode = ({episode}) => {
             dispatch(locationAction.getById());
         }, [dispatch]
     )
+
+ const episodehook = useGetLocationHook(url)
+
     return (
 
         <div>
-            {locationById&&locationById.name}
+            {episodehook && episodehook.name}
+            <div>{locationById && <div>
+                <div>{locationById.name}</div>
+                <div>{locationById.type}</div>
+                <div></div>
+            </div>}
+            </div>
             <div>{name}</div>
             <div>{type}</div>
             <button onClick={() => dispatch(locationAction.getById({id}))}>GetByID</button>
